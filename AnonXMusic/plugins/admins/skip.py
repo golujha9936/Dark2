44bb8bf410bc3c7c1aba96fb88922dee64a721ed
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-08 21:51:42 +0530

    Update skip.py

--- AnonXMusic/plugins/admins/skip.py
+++ AnonXMusic/plugins/admins/skip.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
-from AnonXMusic import YouTube, app
+from AnonXMusic import YouTube, app, YTB
 from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import db
 from AnonXMusic.utils.database import get_loop
@@ -139,7 +139,15 @@ async def skip(cli, message: Message, _, chat_id):
                 video=status,
             )
         except:
-            return await mystic.edit_text(_["call_6"])
+            try:
+                file_path, direct = await YTB.download(
+                    videoid,
+                    mystic,
+                    videoid=True,
+                    video=status,
+                )
+            except:
+                return await mystic.edit_text(_["call_6"])
         try:
             image = await YouTube.thumbnail(videoid, True)
         except:
