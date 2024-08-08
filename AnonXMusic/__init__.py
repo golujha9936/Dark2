cf437dc1732d3d3365c931d5fbc0df73b51b806a
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-08-08 13:41:32 +0530

    __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -17,10 +17,12 @@ userbot = Userbot()
 
 from .platforms import *
 
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+YTB = YTM()
+HELPABLE = {}
